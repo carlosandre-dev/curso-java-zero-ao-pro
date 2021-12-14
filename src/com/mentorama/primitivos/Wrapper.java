70e3786e5bb716673d5fa913ec369a658a0b072e
carlosandre-dev <andre.tjlba@gmail.com>
2021-12-14 19:23:32 -0300

    Desafio classe Wrapper

--- src/com/mentorama/primitivos/Wrapper.java
+++ src/com/mentorama/primitivos/Wrapper.java
@@ -1,2 +1,16 @@
-package com.mentorama.primitivos;public class wrapper {
+package com.mentorama.primitivos;
+
+public class Wrapper {
+    public static void main(String[] args) {
+
+        Integer minhaIdade = 41;
+        Double salario = 1750.00;
+        Float saldoBanco = 400.00F;
+        Character sexo = 'M';
+
+        System.out.println(minhaIdade);
+        System.out.println(salario);
+        System.out.println(saldoBanco);
+        System.out.println(sexo);
+    }
 }
